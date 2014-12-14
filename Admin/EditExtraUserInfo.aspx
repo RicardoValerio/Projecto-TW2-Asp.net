0eb6e12974a3a083c8f12ebec5ea910253b6f9f3
Ricardo Valério <jricvalerio@gmail.com>
2014-12-14 02:49:20 +0000

    select user_formacao in EditUserExtraInfo file

--- Admin/EditExtraUserInfo.aspx
+++ Admin/EditExtraUserInfo.aspx
@@ -30,7 +30,7 @@
     <hr />
     <%-- START SKILLS --%>
     <h2>User Skills</h2>
-    <asp:GridView class="table" Style="margin: 0 auto;" ID="GridView1" AllowPaging="True" PageSize="5" DataKeyNames="ID_Skill" OnPageIndexChanging="OnPagingSkills" AllowSorting="True" runat="server" AutoGenerateColumns="False"
+    <asp:GridView class="table" Style="margin: 0 auto;" ID="GridViewSkills" AllowPaging="True" PageSize="5" DataKeyNames="ID_Skill" OnPageIndexChanging="OnPagingSkills" AllowSorting="True" runat="server" AutoGenerateColumns="False"
         OnRowDataBound="OnRowDataBoundSkills" OnRowEditing="OnRowEditingSkills" OnRowCancelingEdit="OnRowCancelingEditSkills"
         OnRowUpdating="OnRowUpdatingSkills" OnRowDeleting="OnRowDeletingSkills" EmptyDataText="No records has been added.">
         <Columns>
@@ -106,6 +106,140 @@
 
 
     <%-- START FORMAÇÃO --%>
+    <h2>User Formação</h2>
+    <asp:GridView class="table" Style="margin: 0 auto;" ID="GridViewFormacao" AllowPaging="True" PageSize="5" OnPageIndexChanging="OnPagingFormacao" AllowSorting="True" runat="server" AutoGenerateColumns="False"
+        OnRowDataBound="OnRowDataBoundFormacao" OnRowEditing="OnRowEditingFormacao" OnRowCancelingEdit="OnRowCancelingEditFormacao"
+        OnRowUpdating="OnRowUpdatingFormacao" OnRowDeleting="OnRowDeletingFormacao" EmptyDataText="No records has been added." >
+        <Columns>
+
+            <asp:TemplateField HeaderText="Data Inicio" ItemStyle-Width="140">
+
+                <ItemTemplate>
+                    <asp:Label  ID="lblDataInicio" runat="server" Text='<%# Eval("Data_Inicio", "{0:d}") %>'></asp:Label>
+                </ItemTemplate>
+
+                <EditItemTemplate>
+                    <asp:TextBox  ID="txtDataInicio" runat="server" Text='<%# Eval("Data_Inicio", "{0:d}") %>'></asp:TextBox>
+                </EditItemTemplate>
+
+                <ItemStyle Width="140px"></ItemStyle>
+
+            </asp:TemplateField>
+                    <%--------------------------------------------------------%>
+            <asp:TemplateField HeaderText="Data Fim" ItemStyle-Width="140">
+
+                <ItemTemplate>
+                    <asp:Label ID="lblDataFim" runat="server" Text='<%# Eval("Data_Fim", "{0:d}") %>'></asp:Label>
+                </ItemTemplate>
+
+                <EditItemTemplate>
+                    <asp:TextBox ID="txtDataFim" runat="server" Text='<%# Eval("Data_Fim", "{0:d}") %>'></asp:TextBox>
+                </EditItemTemplate>
+
+                <ItemStyle Width="140px"></ItemStyle>
+
+            </asp:TemplateField>
+                     <%--------------------------------------------------------%>
+            <asp:TemplateField HeaderText="Curso" ItemStyle-Width="140">
+
+                <ItemTemplate>
+                    <asp:Label ID="lblCurso" runat="server" Text='<%# Eval("Curso") %>'></asp:Label>
+                </ItemTemplate>
+
+                <EditItemTemplate>
+                    <asp:TextBox ID="txtCurso" runat="server" Text='<%# Eval("Curso") %>'></asp:TextBox>
+                </EditItemTemplate>
+
+                <ItemStyle Width="140px"></ItemStyle>
+
+            </asp:TemplateField>
+            <%--------------------------------------------------------%>
+            <asp:TemplateField HeaderText="Entidade" ItemStyle-Width="140">
+
+                <ItemTemplate>
+                    <asp:Label ID="lblEntidade" runat="server" Text='<%# Eval("Entidade") %>'></asp:Label>
+                </ItemTemplate>
+
+                <EditItemTemplate>
+                    <asp:TextBox ID="txtEntidade" runat="server" Text='<%# Eval("Entidade") %>'></asp:TextBox>
+                </EditItemTemplate>
+
+                <ItemStyle Width="140px"></ItemStyle>
+
+            </asp:TemplateField>
+            <%--------------------------------------------------------%>
+            <asp:TemplateField HeaderText="Tipo Curso" ItemStyle-Width="140">
+
+                <ItemTemplate>
+                    <asp:Label ID="lblTipo_Curso" runat="server" Text='<%# Eval("Tipo") %>'></asp:Label>
+                </ItemTemplate>
+
+                <EditItemTemplate>
+                    <asp:TextBox ID="txtTipo_Curso" runat="server" Text='<%# Eval("Tipo") %>'></asp:TextBox>
+                </EditItemTemplate>
+
+                <ItemStyle Width="140px"></ItemStyle>
+
+            </asp:TemplateField>
+
+            <asp:CommandField ButtonType="Link" ShowEditButton="true" ShowDeleteButton="true" ItemStyle-Width="150" ControlStyle-CssClass="btn updadeDeleteButtons">
+                <ControlStyle CssClass="btn updadeDeleteButtons"></ControlStyle>
+
+                <ItemStyle Width="150px"></ItemStyle>
+            </asp:CommandField>
+
+
+        </Columns>
+    </asp:GridView>
+
+    <asp:SqlDataSource ID="SqlDataSource3" runat="server" ConnectionString="<%$ ConnectionStrings:TW2ProjectConnectionString %>" DeleteCommandType="StoredProcedure" SelectCommand="Formacao_CRUD" SelectCommandType="StoredProcedure" UpdateCommandType="StoredProcedure" InsertCommandType="StoredProcedure" UpdateCommand="Formacao_CRUD">
+        <SelectParameters>
+            <asp:Parameter DefaultValue="SELECT" Name="Action" Type="String" />
+            <asp:QueryStringParameter DefaultValue="" Name="ID_User" QueryStringField="UserId" Type="Int32" />
+            <asp:Parameter Name="ID_Curso" Type="Int32" />
+            <asp:Parameter DbType="Date" Name="Data_Inicio" />
+            <asp:Parameter DbType="Date" Name="Data_Fim" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+
+    <hr />
+
+    <table class="table" border="1" style="margin: 0 auto; border-collapse: collapse">
+        <tr>
+            
+            <td style="width: 150px">Data Inicio:<br />
+                <asp:TextBox ID="TBDataInicio1" runat="server" Width="20px" Height="35px" />
+                <asp:TextBox ID="TBDataInicio2" runat="server" Width="20px" Height="35px" />
+                <asp:TextBox ID="TBDataInicio3" runat="server" Width="70px" Height="35px" />
+            </td>
+
+            <td style="width: 150px">Data Fim:<br />
+                <asp:TextBox ID="TBDataFim1" runat="server" Width="20px" Height="35px" />
+                <asp:TextBox ID="TBDataFim2" runat="server" Width="20px" Height="35px" />
+                <asp:TextBox ID="TBDataFim3" runat="server" Width="70px" Height="35px" />
+            </td>
+
+            <td style="width: 150px">Curso:<br />
+                <asp:TextBox ID="TBCurso" runat="server" Width="150px" Height="35px" />
+            </td>
+
+            <td style="width: 150px">Entidade:<br />
+                <asp:TextBox ID="TBEntidade" runat="server" Width="150px" Height="35px" />
+            </td>
+
+            <asp:SqlDataSource ID="SqlDataSourceTiposCurso" runat="server" ConnectionString="<%$ ConnectionStrings:TW2ProjectConnectionString %>" SelectCommand="SELECT * FROM Tipo_Curso"></asp:SqlDataSource>
+
+            <td style="width: 150px">Tipo de Curso:<br />
+                <asp:DropDownList ID="DDLTipoCurso" runat="server" DataSourceID="SqlDataSourceTiposCurso" DataTextField="Tipo" DataValueField="ID_Tipo"></asp:DropDownList>
+            </td>
+
+            <td style="width: 100px">
+                <asp:Button class="btn btn-success" ID="Button1" runat="server" Text="Add" OnClick="InsertFormacao" />
+            </td>
+        </tr>
+    </table>
+
+    <hr />
     <%-- END FORMAÇÃO --%>
 
 
--- Admin/EditExtraUserInfo.aspx.cs
+++ Admin/EditExtraUserInfo.aspx.cs
@@ -40,6 +40,7 @@ public partial class Admin_EditExtraUserInfo : System.Web.UI.Page
                     SetUserSumario();
                     // fazer o bind de todas as tables
                     this.BindGridSkills();
+                    this.BindGridFormacao();
 
                 } else {
                     // se não existir fazer aqueloutro:
@@ -239,8 +240,8 @@ public partial class Admin_EditExtraUserInfo : System.Web.UI.Page
                     sda.SelectCommand = cmd;
                     using ( DataTable dt = new DataTable() ) {
                         sda.Fill( dt );
-                        GridView1.DataSource = dt;
-                        GridView1.DataBind();
+                        GridViewSkills.DataSource = dt;
+                        GridViewSkills.DataBind();
                     }
                 }
             }
@@ -272,14 +273,14 @@ public partial class Admin_EditExtraUserInfo : System.Web.UI.Page
 
     protected void OnRowEditingSkills( object sender, GridViewEditEventArgs e )
     {
-        GridView1.EditIndex = e.NewEditIndex;
+        GridViewSkills.EditIndex = e.NewEditIndex;
         this.BindGridSkills();
     }
 
     protected void OnRowUpdatingSkills( object sender, GridViewUpdateEventArgs e )
     {
-        GridViewRow row = GridView1.Rows[e.RowIndex];
-        int IdSkill = Convert.ToInt32( GridView1.DataKeys[e.RowIndex].Values[0] );
+        GridViewRow row = GridViewSkills.Rows[e.RowIndex];
+        int IdSkill = Convert.ToInt32( GridViewSkills.DataKeys[e.RowIndex].Values[0] );
         string nomeSkill = ( row.FindControl( "txtNomeSkill" ) as TextBox ).Text;
         string descricaoSkill = ( row.FindControl( "txtDescricaoSkill" ) as TextBox ).Text;
 
@@ -297,19 +298,19 @@ public partial class Admin_EditExtraUserInfo : System.Web.UI.Page
                 con.Close();
             }
         }
-        GridView1.EditIndex = -1;
+        GridViewSkills.EditIndex = -1;
         this.BindGridSkills();
     }
 
     protected void OnRowCancelingEditSkills( object sender, EventArgs e )
     {
-        GridView1.EditIndex = -1;
+        GridViewSkills.EditIndex = -1;
         this.BindGridSkills();
     }
 
     protected void OnRowDeletingSkills( object sender, GridViewDeleteEventArgs e )
     {
-        int IdSkill = Convert.ToInt32( GridView1.DataKeys[e.RowIndex].Values[0] );
+        int IdSkill = Convert.ToInt32( GridViewSkills.DataKeys[e.RowIndex].Values[0] );
         string constr = ConfigurationManager.ConnectionStrings["TW2ProjectConnectionString"].ConnectionString;
         using ( SqlConnection con = new SqlConnection( constr ) ) {
             using ( SqlCommand cmd = new SqlCommand( "User_Skills_CRUD" ) ) {
@@ -330,7 +331,7 @@ public partial class Admin_EditExtraUserInfo : System.Web.UI.Page
 
     protected void OnRowDataBoundSkills( object sender, GridViewRowEventArgs e )
     {
-        if ( e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != GridView1.EditIndex ) {
+        if ( e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != GridViewSkills.EditIndex ) {
             ( e.Row.Cells[2].Controls[2] as LinkButton ).Attributes["onclick"] = "return confirm('Do you want to delete this row?');";
         }
     }
@@ -338,8 +339,8 @@ public partial class Admin_EditExtraUserInfo : System.Web.UI.Page
     protected void OnPagingSkills( object sender, GridViewPageEventArgs e )
     {
         this.BindGridSkills();
-        GridView1.PageIndex = e.NewPageIndex;
-        GridView1.DataBind();
+        GridViewSkills.PageIndex = e.NewPageIndex;
+        GridViewSkills.DataBind();
 
     }
 
@@ -350,6 +351,124 @@ public partial class Admin_EditExtraUserInfo : System.Web.UI.Page
     //END EXPERIENCIA
 
     //START FORMAÇÃO
+    private void BindGridFormacao()
+    {
+        string constr = ConfigurationManager.ConnectionStrings["TW2ProjectConnectionString"].ConnectionString;
+        using ( SqlConnection con = new SqlConnection( constr ) ) {
+            using ( SqlCommand cmd = new SqlCommand( "Formacao_CRUD" ) ) {
+                cmd.Parameters.AddWithValue( "@Action", "SELECT" );
+                cmd.Parameters.AddWithValue( "@ID_User", this.UserId );
+
+                using ( SqlDataAdapter sda = new SqlDataAdapter() ) {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using ( DataTable dt = new DataTable() ) {
+                        sda.Fill( dt );
+                        GridViewFormacao.DataSource = dt;
+                        GridViewFormacao.DataBind();
+                    }
+                }
+            }
+        }
+    }
+
+    protected void InsertFormacao( object sender, EventArgs e )
+    {
+        string skill = DBNomeDaSkill.SelectedValue;
+
+        string constr = ConfigurationManager.ConnectionStrings["TW2ProjectConnectionString"].ConnectionString;
+        using ( SqlConnection con = new SqlConnection( constr ) ) {
+            using ( SqlCommand cmd = new SqlCommand( "Formacao_CRUD" ) ) {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue( "@Action", "INSERT" );
+                cmd.Parameters.AddWithValue( "@ID_User", 1 );
+                cmd.Parameters.AddWithValue( "@ID_Skill", skill );
+                cmd.Parameters.AddWithValue( "@Nome_Skill", "" );
+                cmd.Parameters.AddWithValue( "@Descricao_Skill", "" );
+
+                cmd.Connection = con;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        this.BindGridFormacao();
+    }
+
+    protected void OnRowEditingFormacao( object sender, GridViewEditEventArgs e )
+    {
+        GridViewFormacao.EditIndex = e.NewEditIndex;
+        this.BindGridFormacao();
+    }
+
+    protected void OnRowUpdatingFormacao( object sender, GridViewUpdateEventArgs e )
+    {
+        GridViewRow row = GridViewFormacao.Rows[e.RowIndex];
+        int IdSkill = Convert.ToInt32( GridViewFormacao.DataKeys[e.RowIndex].Values[0] );
+        string nomeSkill = ( row.FindControl( "txtNomeSkill" ) as TextBox ).Text;
+        string descricaoSkill = ( row.FindControl( "txtDescricaoSkill" ) as TextBox ).Text;
+
+        string constr = ConfigurationManager.ConnectionStrings["TW2ProjectConnectionString"].ConnectionString;
+        using ( SqlConnection con = new SqlConnection( constr ) ) {
+            using ( SqlCommand cmd = new SqlCommand( "Formacao_CRUD" ) ) {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue( "@Action", "UPDATE" );
+                cmd.Parameters.AddWithValue( "@ID_Skill", IdSkill );
+                cmd.Parameters.AddWithValue( "@Nome_Skill", nomeSkill );
+                cmd.Parameters.AddWithValue( "@Descricao_Skill", descricaoSkill );
+                cmd.Connection = con;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        GridViewFormacao.EditIndex = -1;
+        this.BindGridFormacao();
+    }
+
+    protected void OnRowCancelingEditFormacao( object sender, EventArgs e )
+    {
+        GridViewFormacao.EditIndex = -1;
+        this.BindGridFormacao();
+    }
+
+    protected void OnRowDeletingFormacao( object sender, GridViewDeleteEventArgs e )
+    {
+        int IdSkill = Convert.ToInt32( GridViewFormacao.DataKeys[e.RowIndex].Values[0] );
+        string constr = ConfigurationManager.ConnectionStrings["TW2ProjectConnectionString"].ConnectionString;
+        using ( SqlConnection con = new SqlConnection( constr ) ) {
+            using ( SqlCommand cmd = new SqlCommand( "Formacao_CRUD" ) ) {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue( "@Action", "DELETE" );
+                cmd.Parameters.AddWithValue( "@ID_User", this.UserId );
+                cmd.Parameters.AddWithValue( "@ID_Skill", IdSkill );
+                cmd.Parameters.AddWithValue( "@Nome_Skill", "" );
+                cmd.Parameters.AddWithValue( "@Descricao_Skill", "" );
+                cmd.Connection = con;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        this.BindGridFormacao();
+    }
+
+    protected void OnRowDataBoundFormacao( object sender, GridViewRowEventArgs e )
+    {
+        if ( e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != GridViewFormacao.EditIndex ) {
+            ( e.Row.Cells[5].Controls[2] as LinkButton ).Attributes["onclick"] = "return confirm('Do you want to delete this row?');";
+        }
+    }
+
+    protected void OnPagingFormacao( object sender, GridViewPageEventArgs e )
+    {
+        this.BindGridFormacao();
+        GridViewFormacao.PageIndex = e.NewPageIndex;
+        GridViewFormacao.DataBind();
+
+    }
+
     //END FORMAÇÃO
 
     //START CERTIFICADOS
